ac43bee4183dfd7afcd96f3473b98ec619400af6
ZabdiAG <aguilera.glz.luis@gmail.com>
2025-06-15 17:50:12 -0600

    add orgmode, delete plugin headlines, neoscroll

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,1 +1,31 @@
-return { "nvim-orgmode/orgmode" }
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup({
+      org_agenda_files = {'~/code/org/*.org'},
+      org_default_notes_file = '~/code/org/default.org',
+      org_capture_templates = {
+       t = {
+         description = 'Task',
+         template = '* TODO %?\n  %u',
+         target = '~/code/org/tasks.org',
+       },
+       n = {
+         description = 'Note',
+         template = '* %u %?\n',
+         target = '~/code/org/notes.org',
+       },
+      },
+    })
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
