a9d342c8e92974da9f0655423cc9245ef8890307
Luis Antonio Aguilera Gonzalez <laguilera@bishopfox.com>
2025-06-18 09:53:35 -0600

    add formatting.lua, lazy-lock updates

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,33 @@
+-- lua/plugins/formatting.lua (or wherever you manage your plugins)
+return {
+  'stevearc/conform.nvim',
+  lazy = false, -- conform.nvim needs to be loaded early for format-on-save
+  keys = {
+    {
+      '<leader>fm',
+      function()
+        require('conform').format({ async = true, lsp_format = 'fallback' })
+      end,
+      mode = 'n',
+      desc = 'Format buffer',
+    },
+  },
+  opts = {
+    -- This is where you configure formatters
+    formatters_by_ft = {
+      go = { 'goimports', 'gofumpt' }, -- We'll define these below
+      -- You can add other languages here
+      -- lua = { 'stylua' },
+      -- python = { 'black' },
+      -- ...
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
+    -- You can also set a default formatter for specific filetypes if needed
+    -- formatters = {
+    --   goimports = {
+    --     -- args = { '-local', 'github.com/your-org' }, -- Example: for goimports -local flag
+    --   },
+    -- },
+  },
+}
