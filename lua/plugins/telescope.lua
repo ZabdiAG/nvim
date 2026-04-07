c7f2f56baeb04cb4549676173b8e6c1a53ad6221
Luis Antonio Aguilera Gonzalez <laguilera@bishopfox.com>
2026-04-07 10:04:01 -0600

    fix: update telescope to latest

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.6",
+  version = '*',
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     -- You can add custom telescope configuration here if needed
