04eb74dc9d47fd408f3afc747bab53942edde71c
Luis Antonio Aguilera Gonzalez <laguilera@bishopfox.com>
2026-04-16 09:56:42 -0600

    update nvim-lsp not needed nvim-lsp config. update colorscheme

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -12,18 +12,6 @@ return {
       capabilities = require('cmp_nvim_lsp').default_capabilities(),
       -- The on_attach function remains the same
       on_attach = function(client, bufnr)
-        -- Key mappings specific to the LSP buffer
-        vim.keymap.set('n', '<leader>ds', vim.diagnostic.open_float, { buffer = bufnr, desc = 'Show diagnostics' })
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = bufnr, desc = 'Go to previous diagnostic' })
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = bufnr, desc = 'Go to next diagnostic' })
-        vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { buffer = bufnr, desc = 'Show diagnostics in quickfix' })
-
-        -- Configure diagnostic signs
-        vim.fn.sign_define('LspDiagnosticsSignError', { text = 'E', texthl = 'DiagnosticSignError' })
-        vim.fn.sign_define('LspDiagnosticsSignWarning', { text = 'W', texthl = 'DiagnosticSignWarn' })
-        vim.fn.sign_define('LspDiagnosticsSignHint', { text = 'H', texthl = 'DiagnosticSignHint' })
-        vim.fn.sign_define('LspDiagnosticsSignInfo', { text = 'I', texthl = 'DiagnosticSignInfo' })
-
         -- Configure diagnostic behavior
         vim.diagnostic.config({
           virtual_text = { spacing = 2, severity_sort = true },
